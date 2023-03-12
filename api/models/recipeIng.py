fa4dc17a114f4cfe05a0bb9b714f0c7fa5481937
Greg Harmon <gregharmon@Gregs-MBP.wirelessinternet>
2023-03-12 15:01:35 -0500

    new push

--- api/models/recipeIng.py
+++ api/models/recipeIng.py
@@ -1,21 +1,21 @@
-from django.db import models
-from .ingredient import Ingredient
+# from django.db import models
+# from .ingredient import Ingredient
 
 
-# Create your models here.
-class RecipeIngredient(models.Model):
-    amount = models.IntegerField()
-    unit = models.CharField(max_length=100)
-    recipe = models.ForeignKey(
-        "Recipe",
-        on_delete=models.CASCADE
-    )
-    ingredients = models.ForeignKey(
-        Ingredient,
-        on_delete=models.CASCADE
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# # Create your models here.
+# class RecipeIngredient(models.Model):
+#     amount = models.IntegerField()
+#     unit = models.CharField(max_length=100)
+#     recipe = models.ForeignKey(
+#         "Recipe",
+#         on_delete=models.CASCADE
+#     )
+#     ingredients = models.ForeignKey(
+#         Ingredient,
+#         on_delete=models.CASCADE
+#     )
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.amount} {self.unit}"
+#     def __str__(self):
+#         return f"{self.amount} {self.unit}"
